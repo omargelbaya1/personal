485b7c5e71adfb4601b5549ab3d267cfda6675f8
Omar Gelbaya <omargelbaya1@gmail.com>
2025-12-14 19:45:36 +0000

    i love python

---
+++ projects/python/courseproj/day19/etchasketch.py
@@ -0,0 +1,38 @@
+import turtle
+from turtle import Turtle , Screen
+
+tim=Turtle()
+
+screen=Screen()
+
+def move_forwards():
+    tim.forward(10)
+
+def move_backwards():
+    tim.back(10)
+
+def move_right():
+    tim.right(10)
+
+def move_left():
+    tim.left(10)
+
+def end():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+
+screen.listen()
+screen.onkey(key="w",fun=move_forwards)
+screen.onkey(key="a",fun=move_left)
+screen.onkey(key="s",fun=move_backwards)
+screen.onkey(key="d",fun=move_right)
+screen.onkey(key='c',fun=end)
+
+
+
+
+
+screen.exitonclick()
--- projects/python/courseproj/day19/main.py
+++ projects/python/courseproj/day19/main.py
@@ -1,38 +1,34 @@
-import turtle
+import random
 from turtle import Turtle , Screen
-
-tim=Turtle()
-
+is_race_on=False
 screen=Screen()
 
-def move_forwards():
-    tim.forward(10)
-
-def move_backwards():
-    tim.back(10)
-
-def move_right():
-    tim.right(10)
-
-def move_left():
-    tim.left(10)
-
-def end():
-    tim.clear()
-    tim.penup()
-    tim.home()
-    tim.pendown()
+screen.setup(width=500,height=400)
+bet=screen.textinput(prompt="Which turtle do u fancy winning mate??",title="most winningest turtle bruh")
+colors=["red","blue","green","yellow","pink","purple"]
+if bet:
+    is_race_on=True
+y=-100
+all_turtles=[]
+for i in colors:
+    new_turtle=Turtle(shape="turtle")
+    all_turtles.append(new_turtle)
+    new_turtle.color(i)
+    new_turtle.penup()
+    new_turtle.goto(x=-235, y=y)
+    y+=40
 
 
-screen.listen()
-screen.onkey(key="w",fun=move_forwards)
-screen.onkey(key="a",fun=move_left)
-screen.onkey(key="s",fun=move_backwards)
-screen.onkey(key="d",fun=move_right)
-screen.onkey(key='c',fun=end)
-
-
-
-
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor()>=230:
+            if bet==turtle.pencolor():
+                print(f"youve won mate, the best turtle was {turtle.pencolor()}")
+                is_race_on=False
+            else:
+                print(f"ur dumb bro, winning turtle was {turtle.pencolor()}")
+                is_race_on=False
+        rand_dist=random.randint(0,10)
+        turtle.forward(rand_dist)
 
 screen.exitonclick()
