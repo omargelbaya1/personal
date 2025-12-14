9185920e4039318db1611a571cbdbae6fb368fda
Omar Gelbaya <omargelbaya1@gmail.com>
2025-12-14 17:19:09 +0000

    learning some more OOP- turtle time!!

---
+++ projects/python/courseproj/day18-turtle/main.py
@@ -0,0 +1,15 @@
+from turtle import Turtle , Screen
+
+tim= Turtle()
+tim.shape("turtle")
+tim.color("red")
+tim.forward(100)
+tim.right(90)
+tim.forward(100)
+tim.right(90)
+tim.forward(100)
+tim.right(90)
+tim.forward(100)
+
+screen=Screen()
+screen.exitonclick()
