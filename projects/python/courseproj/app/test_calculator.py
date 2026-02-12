6106e5ac6e0bd65018618d6a6370a26d5fc8b155
Omar Gelbaya <omargelbaya1@gmail.com>
2026-02-12 09:43:40 +0000

    practicing some testing etc. not sure it works fully, but will probably do more depending on my interview.

---
+++ projects/python/courseproj/app/test_calculator.py
@@ -0,0 +1,23 @@
+import unittest
+
+# ----- code to test -----
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+
+# ----- unit tests -----
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(10, 4), 6)
+        self.assertEqual(subtract(0, 5), -5)
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
