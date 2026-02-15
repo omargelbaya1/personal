d85b85e6db1c9b52a2270ee8937ca31d7761b69b
Omar Gelbaya <omargelbaya1@gmail.com>
2026-02-15 14:22:46 +0000

    learning more about sheety, building a flight tracker using api, i think i need to use the telegram api as twilio is a bit of an expensive solution!

---
+++ projects/python/courseproj/flight-deals-start/data_manager.py
@@ -0,0 +1,4 @@
+class DataManager:
+    #This class is responsible for talking to the Google Sheet.
+    def __init__(self):
+        pass
--- projects/python/courseproj/health-app-spreadsheet/main.py
+++ projects/python/courseproj/health-app-spreadsheet/main.py
@@ -1,78 +1,85 @@
+#CURRENTLY, RUN THIS CODE THROUGH THE TERMINAL E.G. PYTHON3 MAIN.PY, the play button isnt working right now!
+
+
+
+
+
+
 import requests
+import datetime as dt
+import os
 
-APP_ID="app_17e2207c3b4e488f8d18080c"
-API_KEY="nix_live_QY5tlaFc8FkUFsJV2AzoVKAcS0a75UfC"
 
+APP_ID=os.environ["APP_ID"]
+API_KEY=os.environ["API_KEY"]
+TOKEN=os.environ["TOKEN"]
+
+# APP_ID="app_17e2207c3b4e488f8d18080c"
+# API_KEY="nix_live_QY5tlaFc8FkUFsJV2AzoVKAcS0a75UfC"
+# TOKEN="aGFiYXNoaWJhOnIzazk1N2I4"
 headers={
 "x-app-id": APP_ID,
 "x-app-key": API_KEY
 }
 
 response=requests.get("https://app.100daysofpython.dev/healthz",headers=headers)
-print(response.text)
-print(response.json())
 
-# exercise_input=input("Tell me which exercise you did today")
-#
-# parameters={
-#
-#   "query": exercise_input,
-#   "weight_kg": 70,
-#   "height_cm": 175,
-#   "age": 30,
-#   "gender": "male"
-# }
-#
-#
-#
-# calorie_burned=requests.post("https://app.100daysofpython.dev/v1/nutrition/natural/exercise",headers=headers,json=parameters)
-#
-#
-# print(calorie_burned.json())
-# print(calorie_burned.text)
-#
-#
-# data= calorie_burned.json()
+exercise_input=input("Tell me which exercise you did today")
 
-
-parameters_sheety={
-  "sheet1": [
-    {
-      "date": "21/07/2020",
-      "time": "15:00:00",
-      "exercise": "Running",
-      "duration": 22,
-      "calories": 130,
-      "id": 3
-    }
-  ]
+parameters={
+  "query": exercise_input,
+  "weight_kg": 140,
+  "height_cm": 183,
+  "age": 28,
+  "gender": "male"
 }
 
 
-sheety_get=requests.get("https://api.sheety.co/f878894ca96eedd8055b5a328db0a5d3/workouts/sheet1")
-sheety_get.raise_for_status()
-print(sheety_get.json())
-print(sheety_get.text)
-sheety_response=requests.post("https://api.sheety.co/f878894ca96eedd8055b5a328db0a5d3/workouts/sheet1",json=parameters_sheety,headers=None)
+
+calorie_burned=requests.post("https://app.100daysofpython.dev/v1/nutrition/natural/exercise",headers=headers,json=parameters)
+calorie_burned.raise_for_status()
+
+data= calorie_burned.json()
+
+
+current_day=dt.datetime.now().strftime("%d/%m/%Y")
+current_time=dt.datetime.now().strftime("%H:%M:%S")
+name_of_exercise=data["exercises"][0]["name"]
+duration=data["exercises"][0]["duration_min"]
+calories=data["exercises"][0]["nf_calories"]
+
+
+
+
+
+#it will not work if you put [] array brackets around the parameters_sheety parameters.
+parameters_sheety={
+  "workout":
+    {
+      "date":current_day,
+      "time": current_time,
+      "exercise": name_of_exercise,
+      "duration": duration,
+      "calories": calories,
+      "id": 3
+    }
+
+}
+
+headers={
+    # "Authorization":"Basic aGFiYXNoaWJhOnIzazk1N2I4"
+"Authorization":f"Basic {TOKEN}"
+}
+
+
+
+sheety_response=requests.post("https://api.sheety.co/f878894ca96eedd8055b5a328db0a5d3/myWorkouts/workouts",json=parameters_sheety,headers=headers)
 sheety_response.raise_for_status()
 print(sheety_response.text)
 print(sheety_response.json())
 
 
-
-# for i in data["exercises"]:
-#     exercise=i[1]["name"]
-#     exercise_activity=i[0]["user_input"]
-#     with open(f"letter_templates/", "w") as f:
-#         f.write(exercise)
-#         f.write(exercise_activity)
-#
-#
-# parameters={
-#
-#   "query": "ran 3 miles",
-#   "weight_kg": 70,                  // Optional: Weight in kg (1-500)
-#   "height_cm": 175,                 // Optional: Height in cm (1-300)
-#   "age": 30,                        // Optional: Age (1-150)
-#   "gender": "male"                  // Optional: "male" or "female"
-# }
+# sheety_get=requests.get("https://api.sheety.co/f878894ca96eedd8055b5a328db0a5d3/myWorkouts/workouts")
+# sheety_get.raise_for_status()
+# print(sheety_get.json())
+# print(sheety_get.text)
