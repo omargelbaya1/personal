a57155569766582a93d7b7ff5bbd781c6672157e
Omar Gelbaya <omargelbaya1@gmail.com>
2026-02-24 18:19:29 +0000

    spotify api + beautiful soup

--- projects/web-dev/scrape-billboard-100/main.py
+++ projects/web-dev/scrape-billboard-100/main.py
@@ -21,7 +21,7 @@ print(song_names)
 
 
 
-
+# why is python so difficult
 
 scope = "user-library-read"
 
@@ -31,3 +31,6 @@ results = sp.current_user_saved_tracks()
 for idx, item in enumerate(results['items']):
     track = item['track']
     print(idx, track['artists'][0]['name'], " – ", track['name'])
+
+
+    
