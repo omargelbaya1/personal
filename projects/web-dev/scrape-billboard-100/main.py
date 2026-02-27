f2cf6fbec1f6af1f2a2b4bd711b355bcfa3446d0
Omar Gelbaya <omargelbaya1@gmail.com>
2026-02-27 14:27:53 +0000

    i cant seem to figure out the credentials for spootify

--- projects/web-dev/scrape-billboard-100/main.py
+++ projects/web-dev/scrape-billboard-100/main.py
@@ -28,14 +28,14 @@ print(song_names)
 
 
 
-scope = "user-library-read"
+# scope = "user-library-read"
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+# sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-results = sp.current_user_saved_tracks()
-for idx, item in enumerate(results['items']):
-    track = item['track']
-    print(idx, track['artists'][0]['name'], " – ", track['name'])
+# results = sp.current_user_saved_tracks()
+# for idx, item in enumerate(results['items']):
+#     track = item['track']
+#     print(idx, track['artists'][0]['name'], " – ", track['name'])
 
 
     
