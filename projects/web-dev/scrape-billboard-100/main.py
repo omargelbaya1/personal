044e1667be341f3b63ff57cbb79324ce6b5cffc7
Omar Gelbaya <omargelbaya1@gmail.com>
2026-03-01 14:25:34 +0000

    learning about life and everything that come with it

--- projects/web-dev/scrape-billboard-100/main.py
+++ projects/web-dev/scrape-billboard-100/main.py
@@ -19,6 +19,10 @@ song_names = [song.getText().strip() for song in song_names_spans]
 
 print(song_names)
 print(song_names)
+print(song_names)
+print(song_names)
+print(song_names)
+print(song_names)
 
 
 
