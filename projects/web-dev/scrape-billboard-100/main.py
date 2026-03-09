1274f152076ef65dc3b6c2539f8b010163d135dd
Omar Gelbaya <omargelbaya1@gmail.com>
2026-03-09 15:44:11 +0000

    adding some documentation to help me in my job search

--- projects/web-dev/scrape-billboard-100/main.py
+++ projects/web-dev/scrape-billboard-100/main.py
@@ -20,6 +20,11 @@ song_names = [song.getText().strip() for song in song_names_spans]
 
 
 # print(song_names)
+# print(song_names)
+# print(song_names)
+# print(song_names)
+# print(song_names)
+
 
 
 
