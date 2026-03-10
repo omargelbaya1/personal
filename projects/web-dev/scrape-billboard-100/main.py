4781b2b604c3b73f2ef3b336402e60cd229f320b
Omar Gelbaya <omargelbaya1@gmail.com>
2026-03-10 19:43:29 +0000

    il be with u till dusk till dawn

--- projects/web-dev/scrape-billboard-100/main.py
+++ projects/web-dev/scrape-billboard-100/main.py
@@ -20,15 +20,6 @@ song_names = [song.getText().strip() for song in song_names_spans]
 
 
 # print(song_names)
-# print(song_names)
-# print(song_names)
-# print(song_names)
-# print(song_names)
-
-
-
-
-
 year = date.split("-")[0]
 
 
