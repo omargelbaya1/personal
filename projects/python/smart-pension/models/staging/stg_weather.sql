ec56ff16cebd686664f03a9a046d8d655c1a3f0c
Omar Gelbaya <omargelbaya1@gmail.com>
2026-04-14 13:39:55 +0100

    creating some dbt files for moving raw data from my postgre database into my

--- projects/python/smart-pension/models/staging/stg_weather.sql
+++ projects/python/smart-pension/models/staging/stg_weather.sql
@@ -2,6 +2,20 @@ with source as (
 
     select * from {{ source('public', 'raw_weather') }}
 
+),
+
+stg as (
+
+    select distinct on (city,date)
+        city::text                 as city,
+        date::date                 as date,
+        temp_max_c::float          as temp_max_c,
+        temp_min_c::float          as temp_min_c,
+        precipitation_mm::float    as precipitation_mm,
+        ingested_at::timestamp     as ingested_at
+    from source
+    where date is not null
+
 )
 
-select * from source
+select * from stg
